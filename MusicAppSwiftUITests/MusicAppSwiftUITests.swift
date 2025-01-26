5645190268cf33fe2a7087d1e9a0251efe63acd9
saikumar784243 <saikumar784243@gmail.com>
2025-01-26 16:36:46 +0530

    firebase package manager is also added Launchscreen and signup screen ui completed
    Launch screen and signup screen ui elements have done
    when login button is pressed it will login data and signup button is clicked it will show signup screen data using state and binding properties

---
+++ MusicAppSwiftUITests/MusicAppSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppSwiftUITests.swift
+//  MusicAppSwiftUITests
+//
+//  Created by sai kumar on 15/01/25.
+//
+
+import Testing
+@testable import MusicAppSwiftUI
+
+struct MusicAppSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
